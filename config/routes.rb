3267543d5c7b95942f932ede8a83b46eaac697db
JeremyHJDW <jeremy.doukhan@elikhan.fr>
2018-06-26 15:22:15 +0200

    tralala

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'business_cases/edit', to: 'business_cases#editt'
   resources :business_cases
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
