5ad9613bade821da7b19782db6c071d558c2364c
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-10-24 11:58:37 +0200

    Fixed controller tests

--- spec/controllers/business_case_controller_spec.rb
+++ spec/controllers/business_case_controller_spec.rb
@@ -1,19 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe BusinessCasesController, type: :controller do
-  before(:each) do
-    @b = BusinessCase.new
-    @b.title = "Comment tester"
-    @b.client_location = BusinessCase.client_locations_list.sample
-    @b.client_industry = BusinessCase.client_industries_list.sample
-    @b.client_size = BusinessCase.client_sizes_list.sample
-    @b.user_expertise = BusinessCase.user_expertises_list.sample
-    @b.client_position = BusinessCase.client_positions_list.sample
-    @b.user_methodology = "L'instinct"
-    @b.client_problem = "Pas assez de rentabilité"
-    @b.client_results = "Il a fait x2 à la fin !!!"
-    @b.save
+  let(:business_case) do
+    create(:valid_business_case)
   end
+
   describe "GET #index" do
     it "returns a 200 custom status code" do
       get :index
@@ -22,14 +13,13 @@ RSpec.describe BusinessCasesController, type: :controller do
   end
   describe "GET #show" do
     it "returns a 200 custom status code" do
-      get :show, params: { id: @b.id }
-      byebug
+      get :show, params: { id: business_case.id }
       expect(response).to have_http_status(200)
     end
   end
   describe "GET #edit" do
     it "returns a 200 custom status code" do
-      get :edit, params: { id: @b.id }
+      get :edit, params: { id: business_case.id }
       expect(response).to have_http_status(200)
     end
   end
