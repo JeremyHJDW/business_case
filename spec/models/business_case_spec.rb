fab8012eade3b4c61af1681675d9f3b6e2df145e
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-10-24 12:19:04 +0200

    Modified BusinessCase Model tests

--- spec/models/business_case_spec.rb
+++ spec/models/business_case_spec.rb
@@ -2,131 +2,129 @@ require 'rails_helper'
 
 RSpec.describe BusinessCase, type: :model do
 
-  subject { BusinessCase.new }
+  let(:invalid_business_case) { build(:empty_business_case) }
+  let(:business_case) { build(:valid_business_case) }
 
   context "Invalid parameters" do
+    context 'when title is invalid' do
+      it "detects title absence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:title]).to include("can't be blank")
+      end
 
-    it "detects title presence" do
-      subject.title = nil
-      subject.valid?
-      expect(subject.errors[:title]).to include("can't be blank")
+      it "detects an invalid title" do
+        invalid_business_case.title = "This is a test title"
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:title]).to include("Le titre doit commencer par \"Comment \" [suivi par la description du problème résolu dans ce business case]")
+      end
     end
 
-    it "detects an invalid title" do
-      subject.title = "This is a test title"
-      subject.valid?
-      expect(subject.errors[:title]).to include("Le titre doit commencer par \"Comment \" [suivi par la description du problème résolu dans ce business case]")
+
+    context 'when title is invalid' do
+      it "detects location absence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_location]).to include("can't be blank")
+      end
+
+      it "detects an invalid location" do
+        invalid_business_case.client_location = "This is a test location"
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_location]).to include("C'est étonant, mais ton choix de localisation n'appartient pas à la liste autorisée !")
+      end
     end
 
+    context 'when industry is invalid' do
+      it "detects industry absence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_industry]).to include("can't be blank")
+      end
 
-    it "detects location presence" do
-      subject.client_location = nil
-      subject.valid?
-      expect(subject.errors[:client_location]).to include("can't be blank")
+      it "detects an invalid industry" do
+        invalid_business_case.client_industry = "This is a test industry"
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_industry]).to include("C'est étonant, mais ton choix d'industrie n'appartient pas à la liste autorisée !")
+      end
     end
 
-    it "detects an invalid location" do
-      subject.client_location = "This is a test location"
-      subject.valid?
-      expect(subject.errors[:client_location]).to include("C'est étonant, mais ton choix de localisation n'appartient pas à la liste autorisée !")
+    context 'when expertise is invalid' do
+      it "detects expertise absence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:user_expertise]).to include("can't be blank")
+      end
+
+      it "detects an invalid expertise" do
+        invalid_business_case.user_expertise = "This is a test expertise"
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:user_expertise]).to include("C'est étonant, mais ton choix d'expertise n'appartient pas à la liste autorisée !")
+      end
     end
 
-    it "detects industry presence" do
-      subject.client_industry = nil
-      subject.valid?
-      expect(subject.errors[:client_industry]).to include("can't be blank")
+    context 'when size is invalid' do
+      it "detects size absence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_size]).to include("can't be blank")
+      end
+
+      it "detects an invalid size" do
+        invalid_business_case.client_size = "This is a test size"
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_size]).to include("C'est étonant, mais ton choix de taille d'entreprise n'appartient pas à la liste autorisée !")
+      end
     end
 
-    it "detects an invalid industry" do
-      subject.client_industry = "This is a test industry"
-      subject.valid?
-      expect(subject.errors[:client_industry]).to include("C'est étonant, mais ton choix d'industrie n'appartient pas à la liste autorisée !")
+    context 'when position is invalid' do
+      it "detects position absence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_position]).to include("can't be blank")
+      end
+
+      it "detects an invalid position" do
+        invalid_business_case.client_position = "This is a test position"
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_position]).to include("C'est étonant, mais ton choix de fonction de l'interlocuteur client n'appartient pas à la liste autorisée !")
+      end
     end
 
-    it "detects expertise presence" do
-      subject.user_expertise = nil
-      subject.valid?
-      expect(subject.errors[:user_expertise]).to include("can't be blank")
+    context 'when methology is invalid' do
+      it "detects methodology absence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:user_methodology]).to include("can't be blank")
+      end
     end
 
-    it "detects an invalid expertise" do
-      subject.user_expertise = "This is a test expertise"
-      subject.valid?
-      expect(subject.errors[:user_expertise]).to include("C'est étonant, mais ton choix d'expertise n'appartient pas à la liste autorisée !")
+    context 'when problem is invalid' do
+      it "detects problem presence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_problem]).to include("can't be blank")
+      end
     end
 
-    it "detects size presence" do
-      subject.client_size = nil
-      subject.valid?
-      expect(subject.errors[:client_size]).to include("can't be blank")
+    context 'when results is invalid' do
+      it "detects results presence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:client_results]).to include("can't be blank")
+      end
     end
 
-    it "detects an invalid size" do
-      subject.client_size = "This is a test size"
-      subject.valid?
-      expect(subject.errors[:client_size]).to include("C'est étonant, mais ton choix de taille d'entreprise n'appartient pas à la liste autorisée !")
+    context 'when user is invalid' do
+      it "detects user presence" do
+        invalid_business_case.valid?
+        expect(invalid_business_case.errors[:user]).to include("must exist")
+      end
     end
-
-    it "detects position presence" do
-      subject.client_position = nil
-      subject.valid?
-      expect(subject.errors[:client_position]).to include("can't be blank")
-    end
-
-    it "detects an invalid position" do
-      subject.client_position = "This is a test position"
-      subject.valid?
-      expect(subject.errors[:client_position]).to include("C'est étonant, mais ton choix de fonction de l'interlocuteur client n'appartient pas à la liste autorisée !")
-    end
-
-    it "detects methodology presence" do
-      subject.user_methodology = nil
-      subject.valid?
-      expect(subject.errors[:user_methodology]).to include("can't be blank")
-    end
-
-    it "detects problem presence" do
-      subject.client_problem = nil
-      subject.valid?
-      expect(subject.errors[:client_problem]).to include("can't be blank")
-    end
-
-    it "detects results presence" do
-      subject.client_results = nil
-      subject.valid?
-      expect(subject.errors[:client_results]).to include("can't be blank")
-    end
-
   end
 
-
-
   context "Valid parameters" do
-
     it "creates a business case with valid parameters" do
-      subject.title = "Comment tester"
-      subject.client_location = BusinessCase.client_locations_list.sample
-      subject.client_industry = BusinessCase.client_industries_list.sample
-      subject.client_size = BusinessCase.client_sizes_list.sample
-      subject.user_expertise = BusinessCase.user_expertises_list.sample
-      subject.client_position = BusinessCase.client_positions_list.sample
-      subject.user_methodology = "L'instinct"
-      subject.client_problem = "Pas assez de rentabilité"
-      subject.client_results = "Il a fait x2 à la fin !!!"
-
-      subject.valid?
-      expect(subject).to be_valid
-
-
-      subject.save
-
-      subject.pending!
-      expect(subject.client_validation_status).to eq("pending")
+      business_case.valid?
+      expect(business_case).to be_valid
     end
+    #invalid_business_case.pending!
+    #expect(invalid_business_case.client_validation_status).to eq("pending")
 
     # it "changes comment status" do
-    #   subject.comment_pending!
-    #   expect(subject.client_comment_status).to eq "pending"
+    #   invalid_business_case.comment_pending!
+    #   expect(invalid_business_case.client_comment_status).to eq "pending"
     # end
 
   end
