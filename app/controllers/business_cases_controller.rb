69d0ffe9e14fc83c1724b9702f71d4f514a30b43
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-10-25 12:22:46 +0200

    From now, we display only user's business cases

--- app/controllers/business_cases_controller.rb
+++ app/controllers/business_cases_controller.rb
@@ -6,7 +6,7 @@ class BusinessCasesController < ApplicationController
 
   # GET /business_cases
   def index
-    @business_cases = BusinessCase.all
+    @business_cases = current_user.business_cases
   end
 
   # GET /business_cases/1
